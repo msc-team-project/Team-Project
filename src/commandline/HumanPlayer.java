e8f0b205a1bc62abd1cf5c415365415599171a64
mtmurray <35267661+mtmurray@users.noreply.github.com>
2018-01-23 21:57:57 +0000

    Add files via upload

---
+++ src/commandline/HumanPlayer.java
@@ -0,0 +1,34 @@
+package commandline;
+
+import java.util.Scanner;
+
+public class HumanPlayer extends Player{
+
+	//prompts player to select an attribute
+	//takes a scanner as an argument so that the main scanner can be reused
+	//having multiple scanners open at once seems to cause errors
+	public String playerSelectAttribute(Scanner scanner)
+	{
+		String att = "";
+		String[] atts = cardInPlay.getAttributes();
+		boolean valid = false;
+		String message = "Select attribute: ";
+		while(!valid)
+		{
+			//checks that the input is one of the card's attribute names
+			System.out.println(message);
+			att = scanner.next();
+			for(String s : atts)
+			{
+				if(att.toLowerCase().equals(s))
+					valid = true;
+				else if (att.equals("quit") || att.equals("exit"))
+					System.exit(0);
+			}
+			message = "Please enter a valid attribute (or 'quit' to quit)";
+		}		
+		//returns the attribute as a String
+		return att;
+	}
+	
+}
---
+++ src/online/dwResources/TopTrumpsRESTAPI.java
@@ -0,0 +1,171 @@
+package online.dwResources;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import online.configuration.TopTrumpsJSONConfiguration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import commandline.AIPlayer;
+import commandline.Card;
+import commandline.HumanPlayer;
+import commandline.Player;
+
+@Path("/toptrumps") // Resources specified here should be hosted at http://localhost:7777/toptrumps
+@Produces(MediaType.APPLICATION_JSON) // This resource returns JSON content
+@Consumes(MediaType.APPLICATION_JSON) // This resource can take JSON content as input
+/**
+ * This is a Dropwizard Resource that specifies what to provide when a user
+ * requests a particular URL. In this case, the URLs are associated to the
+ * different REST API methods that you will need to expose the game commands
+ * to the Web page.
+ * 
+ * Below are provided some sample methods that illustrate how to create
+ * REST API methods in Dropwizard. You will need to replace these with
+ * methods that allow a TopTrumps game to be controled from a Web page.
+ */
+public class TopTrumpsRESTAPI {
+
+	/** A Jackson Object writer. It allows us to turn Java objects
+	 * into JSON strings easily. */
+	ObjectWriter oWriter = new ObjectMapper().writerWithDefaultPrettyPrinter();
+	
+	/**
+	 * Contructor method for the REST API. This is called first. It provides
+	 * a TopTrumpsJSONConfiguration from which you can get the location of
+	 * the deck file and the number of AI players.
+	 * @param conf
+	 */
+	public TopTrumpsRESTAPI(TopTrumpsJSONConfiguration conf) {
+		// ----------------------------------------------------
+		// Add relevant initalization here
+		// ----------------------------------------------------
+	}
+	
+	// ----------------------------------------------------
+	// Add relevant API methods here
+	// ----------------------------------------------------
+	
+	@GET
+	@Path("/helloJSONList")
+	/**
+	 * Here is an example of a simple REST get request that returns a String.
+	 * We also illustrate here how we can convert Java objects to JSON strings.
+	 * @return - List of words as JSON
+	 * @throws IOException
+	 */
+	public String helloJSONList() throws IOException {
+		
+		List<String> listOfWords = new ArrayList<String>();
+		listOfWords.add("Hello");
+		listOfWords.add("World!");
+		
+		// We can turn arbatory Java objects directly into JSON strings using
+		// Jackson seralization, assuming that the Java objects are not too complex.
+		String listAsJSONString = oWriter.writeValueAsString(listOfWords);
+		
+		return listAsJSONString;
+	}
+	
+	@GET
+	@Path("/buildDeckJSON")
+	public String buildDeckJSON() {
+		System.out.println("buildDeck method reached");
+		//reads in the deck from the txt file
+		ArrayList<Card> deck = new ArrayList<Card>();
+		try
+		{
+			//I had to give the absolute path for this to work in command prompt
+			// eg "C:\\Users\\Roddy\\workspace\\Trumps\\StarCitizenDeck.txt"
+			BufferedReader reader = new BufferedReader(new FileReader("StarCitizenDeck.txt"));
+		
+			
+			String line = reader.readLine();
+			//skip the first line because it is just the names of attributes
+			//will maybe read this line in and change card constructor
+			//so decks with more/less/different attributes can be used
+			line = reader.readLine();
+			while (line != null)
+			{
+				//read each line in and create new card
+				Card card = new Card(line);
+				deck.add(card);
+				line = reader.readLine();
+			}
+			reader.close();
+			//return the built deck as and arraylist of cards
+			String deckString = oWriter.writeValueAsString(deck);
+			return deckString;
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	@GET
+	@Path("/addAIPlayers")
+	public ArrayList<Player> addAIPlayers()
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		//create an arraylist for players and add a human player
+		ArrayList<Player> players = new ArrayList<Player>();
+		Player player = new HumanPlayer();
+		players.add(player);
+		int numberPlayers = players.size();
+		//prompt the user to enter number of ai players
+		while (numberPlayers < 2 || numberPlayers > 5)
+		{
+			try
+			{
+				System.out.println("How many opponents (1-4)");
+				int aiplayers = Integer.parseInt(scanner.nextLine());
+				numberPlayers = 1 + aiplayers;
+			} catch (Exception e)
+			{
+				//this catch handles any integer parsing errors (eg user enters "two" instead of 2 etc)
+				//by re-prompting the user
+				//probably not good practice to handle any and all exceptions by ignoring them
+				//but this can be fixed later
+				continue;
+			}
+		}
+		for (int i = 1; i < numberPlayers; i++)
+		{
+			//create new ai players with the names ai player1, ai player2, etc
+			//and adds to players list
+			String name = String.format("AI Player %d", i);
+			AIPlayer aiPlayer = new AIPlayer(name);
+			players.add(aiPlayer);
+		}
+		return players;
+	}
+	
+	@GET
+	@Path("/helloWord")
+	/**
+	 * Here is an example of how to read parameters provided in an HTML Get request.
+	 * @param Word - A word
+	 * @return - A String
+	 * @throws IOException
+	 */
+	public String helloWord(@QueryParam("Word") String Word) throws IOException {
+		return "Hello "+Word;
+	}
+	
+}
