3d225c8ad5766a2fd2c9dccb368ddc773d41ec07
1102401d <35267084+1102401d@users.noreply.github.com>
2018-02-08 12:43:29 +0000

    Add files via upload

--- TopTrumpsRESTAPI.java
+++ TopTrumpsRESTAPI.java
@@ -4,8 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 import javax.ws.rs.Consumes;
@@ -26,6 +28,7 @@ import commandline.Card;
 import commandline.DataBaseCon;
 import commandline.HumanPlayer;
 import commandline.Player;
+import commandline.TopTrumpsCLIApplication;
 
 @Path("/toptrumps") // Resources specified here should be hosted at http://localhost:7777/toptrumps
 @Produces(MediaType.APPLICATION_JSON) // This resource returns JSON content
@@ -42,10 +45,19 @@ import commandline.Player;
  */
 public class TopTrumpsRESTAPI {
 
+	/** the current round */
+	private static int round;
+	/** maintains a count of the draws in a single game */
+	private static int draws;
+	private static int numberPlayers;
+	private static ArrayList<Player> allPlayers;
+	private static ArrayList<Player> players;
+	private static ArrayList<Card> deck, communalDeck, cardsInPlay;
+	private static Player currentPlayer;
 	/** A Jackson Object writer. It allows us to turn Java objects
 	 * into JSON strings easily. */
 	ObjectWriter oWriter = new ObjectMapper().writerWithDefaultPrettyPrinter();
-	
+
 	/**
 	 * Contructor method for the REST API. This is called first. It provides
 	 * a TopTrumpsJSONConfiguration from which you can get the location of
@@ -56,12 +68,56 @@ public class TopTrumpsRESTAPI {
 		// ----------------------------------------------------
 		// Add relevant initalization here
 		// ----------------------------------------------------
+		/*
+		numberPlayers = conf.getNumAIPlayers()+1;
+		System.err.println(numberPlayers);
+		
+		
+		deck = buildDeck();
+		Collections.shuffle(deck);
+		//create a new empty ArrayList for the communal deck
+
+		communalDeck = new ArrayList<Card>();
+		// set up the players and divide deck between them
+
+		players = setUpPlayers(numberPlayers);
+		//create a second array of players for reference to players after game ends
+		allPlayers = new ArrayList<Player>(players);
+		divideDeck(numberPlayers);
+		round=0;
+		draws=0;
+		int startPlayer = new Random().nextInt(numberPlayers);
+		
+		currentPlayer = players.get(startPlayer);
+		
+			*/
+		
+		
 	}
+
+	
+	@GET
+	@Path("/turn")
+	public String turn() {
+		String turn="";
+		if (currentPlayer instanceof HumanPlayer)
+		{
+			turn = "Your";
+		}
+		else
+		{
+			turn = currentPlayer.getName();
+		}
+		
+		return turn;
+	}
+	
+	
 	
 	// ----------------------------------------------------
 	// Add relevant API methods here
 	// ----------------------------------------------------
-	
+
 	@GET
 	@Path("/helloJSONList")
 	/**
@@ -71,24 +127,74 @@ public class TopTrumpsRESTAPI {
 	 * @throws IOException
 	 */
 	public String helloJSONList() throws IOException {
-		
+
 		List<String> listOfWords = new ArrayList<String>();
 		listOfWords.add("Hello");
 		listOfWords.add("World!");
-		
+
 		// We can turn arbatory Java objects directly into JSON strings using
 		// Jackson seralization, assuming that the Java objects are not too complex.
 		String listAsJSONString = oWriter.writeValueAsString(listOfWords);
-		
+
 		return listAsJSONString;
 	}
+
+	
+	
+	
+
 	
 	@GET
 	@Path("/playGame")
-	public void playGame() 
+	public void playGame(@QueryParam("numPlayers") int numPlayers) 
 	{
+		numberPlayers = numPlayers;
+		System.err.println(numPlayers);
+		System.err.println(numberPlayers);
+		
+		deck = buildDeck();
+		Collections.shuffle(deck);
+		//create a new empty ArrayList for the communal deck
+
+		communalDeck = new ArrayList<Card>();
+		// set up the players and divide deck between them
+
+		players = setUpPlayers(numberPlayers);
+		//create a second array of players for reference to players after game ends
+		allPlayers = new ArrayList<Player>(players);
+		divideDeck(numberPlayers);
+		round=0;
+		draws=0;
+		int startPlayer = new Random().nextInt(numberPlayers);
+		//System.err.println(players);
+		 currentPlayer = players.get(startPlayer);
+		//return numberPlayers;
+	}
+	
+	
+	
+	@GET
+	@Path("/playRound")
+	public ArrayList<Card> playRound() 
+	{
+		round++;
+		cardsInPlay = playNextHand();
+		
+		return cardsInPlay;
+	}
+	
+	
+	
+	@GET
+	@Path("/AIPick")
+	public String AIPick() {
+		String attribute = currentPlayer.pickAttribute();
+		return attribute;
+		
 		
 	}
+	
+	
 	
 	@GET
 	@Path("/quitGame")
@@ -97,10 +203,10 @@ public class TopTrumpsRESTAPI {
 		String quitMessage = "You have ended the game.";
 		return quitMessage;
 	}
-	
+
 	//Will be called from playGame(), rather than being called directly from GameScreen
-	@GET
-	@Path("/buildDeck")
+	//@GET
+	//@Path("/buildDeck")
 	public ArrayList<Card> buildDeck() {
 		//reads in the deck from the txt file
 		ArrayList<Card> deck = new ArrayList<Card>();
@@ -109,7 +215,7 @@ public class TopTrumpsRESTAPI {
 			//I had to give the absolute path for this to work in command prompt
 			// eg "C:\\Users\\Roddy\\workspace\\Trumps\\StarCitizenDeck.txt"
 			BufferedReader reader = new BufferedReader(new FileReader("StarCitizenDeck.txt"));
-		
+
 			String line = reader.readLine();
 			//skip the first line because it is just the names of attributes
 			//will maybe read this line in and change card constructor
@@ -133,34 +239,209 @@ public class TopTrumpsRESTAPI {
 			return null;
 		}
 	}
-	
-	@GET
-	@Path("/setUpPlayers")
-	public Player[] setUpPlayers(@QueryParam("numPlayers") int numPlayers) 
+
+	//@GET
+	//@Path("/setUpPlayers")
+	public ArrayList<Player> setUpPlayers(int numPlayers) 
 	{
-		Player [] players = new Player [numPlayers];
-		
-		HumanPlayer roddy = new HumanPlayer();
-		players[0] = roddy;
-		
-		int opponents = numPlayers - 1;
-		
-		for (int i = 1; i <= opponents; i++) 
+		ArrayList<Player> players = new ArrayList<Player>();
+
+		Player hum = new HumanPlayer();
+		players.add(hum);
+
+		//int opponents = numPlayers - 1;
+
+		for (int i = 1; i <= numPlayers-1; i++) 
 		{
 			String name = String.format("AI Player %d", i);
 			AIPlayer ai = new AIPlayer(name);
-			players[i] = ai;
+			players.add(ai);
 		}
-		
+
 		return players;
 	}
+	
+	private static void divideDeck(int numPlayers) {
+		//sequentially divides the deck among players
+		//not the most efficient way of doing it but it works and was easy to implement
+		int i = 0;
+		for (Card card : deck)
+		{
+			players.get(i % numPlayers).addCardToDeck(card);
+			i++;
+		}
+	}
+	
+	
+	public static ArrayList<Card> playNextHand() {
+		//System.err.println(players);
+		ArrayList<Card> cards = new ArrayList<Card>();
+		for (Player p : players)
+		{
+			cards.add(p.playCard());
+		}
+
+		if (players.get(0) instanceof HumanPlayer)
+		{
+			System.out.println("Your Card:");
+			players.get(0).printCardInPlay();
+		}
+
+		return cards;
+	}
+	
+
+	@GET
+	@Path("/getRoundWinner")
+	public static String getRoundWinner(@QueryParam("getRoundWinner") String att) {
+		System.err.println(att);
+		ArrayList<Player> winners = compareCards(att);
+		String win ="";
+		if (winners.size() == 1)
+		{
+			
+			Player winner = winners.get(0);
+			int newCards = cardsInPlay.size() + communalDeck.size();
+			winner.addHandToDeck(cardsInPlay);
+			winner.addHandToDeck(communalDeck);
+			winner.incrementRoundsWon();
+			currentPlayer = winner;
+			if (winner instanceof HumanPlayer)
+			{
+				win = "You win round " + round+" "+
+				newCards + " cards added to your hand";
+			} 
+			
+			else {
+				win = winner.getName()+" wins round "+ round+" "+
+						newCards + " cards added to their hand";;
+			}
+			//reset the communal deck
+			communalDeck = new ArrayList<Card>();
+		}
+		else
+		{
+			draws++;
+			communalDeck.addAll(cardsInPlay);
+			
+			
+			win = "Draw between ";
+			int i = 0;
+			//check if instance of human player
+			//because the human player is always added to this list first
+			// we only need to check at index 0 for a human
+			if (winners.get(0) instanceof HumanPlayer)
+			{
+				win += "You ";
+				i++;
+			}
+			
+			
+			
+			for (; i < winners.size(); i++)
+			{
+				win += winners.get(i).getName() + " ";
+			}
+		}
+		
+		//printRemainingCards();
+		//checkConditions();
+		return win;
+	}
+	
+	@GET
+	@Path("/deckLeft")
+	public static int[] deckLeft() {
+		
+		int[] deckLeft= new int[5];
+			for(int i =0; i<allPlayers.size() ;i++) {
+			deckLeft[i]=allPlayers.get(i).getDeckSize();	
+		}
+		
+		
+		
+		return deckLeft;
+	}
+	
+	
+	@GET
+	@Path("/checkConditions")
+	public static int checkConditions() {
+		int remove = 0;
+		for(Player p : allPlayers)
+		{
+			if(p.getDeckSize() == 0)
+			{
+				if(p instanceof HumanPlayer)
+				{
+					System.err.println("You have no cards remaining");
+					remove++;
+				}
+				else
+				{
+					System.err.println(p.getName() + " has no cards remaining");
+					remove++;
+				}
+				
+				
+				//remove players with no cards and decrement number of players
+				players.remove(p);
+				numberPlayers--;
+			}
+		}
+		int size = players.size();
+		//System.err.println(remove);
+		return size;
+	}
+	
+	@GET
+	@Path("/winner")
+	public static String winner() {
+		
+		String win ="";
+		if(players.get(0) instanceof HumanPlayer)
+		{
+			
+			win = "You";
+		}
+		else
+		{
+			//cast to ai player and get player name
+			win = players.get(0).getName();
+							
+		}
+		return win;
+	}
+	
+	
+	private static ArrayList<Player> compareCards(String att) {
+		ArrayList<Player> winners = new ArrayList<Player>();
+		int max = 0;
+		//find the max value for the chosen attribute for all cards in play
+		for (Player p : players)
+		{
+			if (p.getCardInPlay().getValueOfAtt(att) > max)
+				max = p.getCardInPlay().getValueOfAtt(att);
+		}
+		//get all players whose card has that value for the attribute
+		//and add them to the list of winners
+		for (Player p : players)
+		{
+			if (p.getCardInPlay().getValueOfAtt(att) == max)
+				winners.add(p);
+		}
+		//return the winners as an arraylist of players
+		return winners;
+	}
+	
+	
 	@GET
 	@Path("/aiChoice")
 	public String aiChoice(@QueryParam("aiCard") Card aiCard) {
 		String[] atts = aiCard.getAttributes();
 		int[] values = {aiCard.getSize(), aiCard.getSpeed(), aiCard.getRange(), aiCard.getFirepower(), aiCard.getCargo()};
 		String att = "";
-	
+
 		int max = 0;
 		for(int i = 0; i < 5; i++)
 		{
@@ -174,18 +455,18 @@ public class TopTrumpsRESTAPI {
 		return att;
 	}
 	//Method to divide the deck based on number of players
-	@GET
-	@Path("/splitDeck")
-	public void splitDeck(@QueryParam("numPlayers") int numPlayers) 
-	{
-		
-	}
+	//@GET
+	//@Path("/splitDeck")
+	//public void splitDeck(@QueryParam("numPlayers") int numPlayers) 
+	//{
+
+	//}
 	@GET
 	@Path("/getStats")
 	public String getStats() throws IOException {
-		
+
 		List<String> stats = new ArrayList<String>();
-		
+
 		DataBaseCon.connect();
 		int numGames = DataBaseCon.numGames();
 		int aiWins = DataBaseCon.aiWins();
@@ -203,12 +484,12 @@ public class TopTrumpsRESTAPI {
 		stats.add(human);
 		stats.add(max);
 		stats.add(draw);
-		
+
 		String statsAsJSONString = oWriter.writeValueAsString(stats);
 		return statsAsJSONString;
-		
+
 	}
-	
+
 	@GET
 	@Path("/helloWord")
 	/**
@@ -220,5 +501,5 @@ public class TopTrumpsRESTAPI {
 	public String helloWord(@QueryParam("Word") String Word) throws IOException {
 		return "Hello "+Word;
 	}
-	
+
 }
